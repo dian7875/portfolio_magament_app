16f0b553043092b47c85b98f898f47be1349bdc4
Dian <dian7875.a@gmail.com>
2026-01-02 14:38:11 -0600

    chore(Filters): Implemented reutililitables components for string filters and select status filter

---
+++ src/shared/components/SelectStatus.vue
@@ -0,0 +1,22 @@
+<template>
+  <FwbSelect
+    id="status"
+    name="status"
+    v-model="uiValue"
+    :options="status"
+    label="Filtro por estado"
+    class="min-w-[10dvw]"
+  />
+</template>
+<script setup lang="ts">
+import { FwbSelect } from "flowbite-vue";
+import { useHiddenFilter } from "../composables/useHiddenFilter";
+
+const { uiValue } = useHiddenFilter();
+
+const status = [
+  { value: "all", name: "Todos" },
+  { value: "false", name: "Visibles" },
+  { value: "true", name: "Ocultos" },
+];
+</script>
---
+++ src/shared/components/StringFilter.vue
@@ -0,0 +1,30 @@
+<template>
+  <FwbInput
+    :id="queryKey"
+    :name="queryKey"
+    v-model="uiValue"
+    :label="`Filtro por ${label}`"
+    :placeholder="placeholder"
+    autocomplete="off"
+    class="min-w-[10dvw] custom-label!"
+  />
+</template>
+
+<script setup lang="ts">
+import { FwbInput } from "flowbite-vue";
+import { useStringFilter } from "../composables/useStringFilter";
+
+const props = defineProps<{
+  queryKey: string;
+  label: string;
+  placeholder?: string;
+}>();
+
+const { uiValue } = useStringFilter(props.queryKey);
+</script>
+
+<style scoped>
+.custom-label label {
+  color: black!;
+}
+</style>
